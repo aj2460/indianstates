407e2e11ce9aa7d029aadb83dc269cb2f7cafb57
Anand <ajkadakkal@gmail.com>
2021-11-25 12:54:12 +0530

    Add short description for states

--- lib/pages/state_list.dart
+++ lib/pages/state_list.dart
@@ -23,32 +23,133 @@ List<Map<String, String>> states = [
     'pic': 'Chhattisgarh.png'
   },
   {'index': '06', 'state': "Goa", 'capital': 'Panaji', 'pic': 'Goa.png'},
-  {'state': "Gujarat", 'capital': 'Gandhinagar', 'pic': 'Gujarat.png'},
-  {'state': "Haryana", 'capital': 'Chandigarh', 'pic': 'Haryana.png'},
-  {'state': "Himachal Pradesh", 'capital': 'Shimla', 'pic': 'Himachal.png'},
-  {'state': "Jharkhand", 'capital': 'Ranchi', 'pic': 'Jharkhand.png'},
-  {'state': "Karnataka", 'capital': 'Bangalore', 'pic': 'Karnataka.png'},
-  {'state': "Kerala", 'capital': 'Thiruvananthapuram', 'pic': 'Kerala.png'},
-  {'state': "Madhya Pradesh", 'capital': 'Bhopal', 'pic': 'Madhya.png'},
-  {'state': "Maharashtra", 'capital': 'Mumbai', 'pic': 'Maharashtra.png'},
-  {'state': "Manipur", 'capital': 'Imphal', 'pic': 'Manipur.png'},
-  {'state': "Meghalaya", 'capital': 'Shillong', 'pic': 'Meghalaya.png'},
-  {'state': "Mizoram", 'capital': 'Aizawl', 'pic': 'Mizoram.png'},
-  {'state': "Nagaland", 'capital': 'Kohima', 'pic': 'Nagaland.png'},
-  {'state': "Odisha", 'capital': 'Bhubaneswar', 'pic': 'Odisha.png'},
-  {'state': "Punjab", 'capital': 'Chandigarh', 'pic': 'Punjab.png'},
-  {'state': "Rajasthan", 'capital': 'Jaipur', 'pic': 'Rajasthan.png'},
-  {'state': "Sikkim", 'capital': 'Gangtok', 'pic': 'Sikkim.png'},
-  {'state': "Tamil Nadu", 'capital': 'Chennai', 'pic': 'Tamil.png'},
-  {'state': "Telangana", 'capital': 'Hyderabad', 'pic': 'Telangana.png'},
-  {'state': "Tripura", 'capital': 'Agartala	', 'pic': 'Tripura.png'},
-  {'state': "Uttar Pradesh", 'capital': 'Lucknow', 'pic': 'Uttar.png'},
   {
+    'index': '07',
+    'state': "Gujarat",
+    'capital': 'Gandhinagar',
+    'pic': 'Gujarat.png'
+  },
+  {
+    'index': '08',
+    'state': "Haryana",
+    'capital': 'Chandigarh',
+    'pic': 'Haryana.png'
+  },
+  {
+    'index': '09',
+    'state': "Himachal Pradesh",
+    'capital': 'Shimla',
+    'pic': 'Himachal.png'
+  },
+  {
+    'index': '10',
+    'state': "Jharkhand",
+    'capital': 'Ranchi',
+    'pic': 'Jharkhand.png'
+  },
+  {
+    'index': '11',
+    'state': "Karnataka",
+    'capital': 'Bangalore',
+    'pic': 'Karnataka.png'
+  },
+  {
+    'index': '12',
+    'state': "Kerala",
+    'capital': 'Thiruvananthapuram',
+    'pic': 'Kerala.png'
+  },
+  {
+    'index': '13',
+    'state': "Madhya Pradesh",
+    'capital': 'Bhopal',
+    'pic': 'Madhya.png'
+  },
+  {
+    'index': '14',
+    'state': "Maharashtra",
+    'capital': 'Mumbai',
+    'pic': 'Maharashtra.png'
+  },
+  {
+    'index': '15',
+    'state': "Manipur",
+    'capital': 'Imphal',
+    'pic': 'Manipur.png'
+  },
+  {
+    'index': '16',
+    'state': "Meghalaya",
+    'capital': 'Shillong',
+    'pic': 'Meghalaya.png'
+  },
+  {
+    'index': '17',
+    'state': "Mizoram",
+    'capital': 'Aizawl',
+    'pic': 'Mizoram.png'
+  },
+  {
+    'index': '18',
+    'state': "Nagaland",
+    'capital': 'Kohima',
+    'pic': 'Nagaland.png'
+  },
+  {
+    'index': '19',
+    'state': "Odisha",
+    'capital': 'Bhubaneswar',
+    'pic': 'Odisha.png'
+  },
+  {
+    'index': '20',
+    'state': "Punjab",
+    'capital': 'Chandigarh',
+    'pic': 'Punjab.png'
+  },
+  {
+    'index': '21',
+    'state': "Rajasthan",
+    'capital': 'Jaipur',
+    'pic': 'Rajasthan.png'
+  },
+  {'index': '22', 'state': "Sikkim", 'capital': 'Gangtok', 'pic': 'Sikkim.png'},
+  {
+    'index': '23',
+    'state': "Tamil Nadu",
+    'capital': 'Chennai',
+    'pic': 'Tamil.png'
+  },
+  {
+    'index': '24',
+    'state': "Telangana",
+    'capital': 'Hyderabad',
+    'pic': 'Telangana.png'
+  },
+  {
+    'index': '25',
+    'state': "Tripura",
+    'capital': 'Agartala	',
+    'pic': 'Tripura.png'
+  },
+  {
+    'index': '26',
+    'state': "Uttar Pradesh",
+    'capital': 'Lucknow',
+    'pic': 'Uttar.png'
+  },
+  {
+    'index': '27',
     'state': "Uttarakhand",
     'capital': 'Dehradun, Gairsain (Summer)',
     'pic': 'Uttarakhand.png'
   },
-  {'state': "West Bengal", 'capital': 'Kolkata', 'pic': 'West.png'},
+  {
+    'index': '28',
+    'state': "West Bengal",
+    'capital': 'Kolkata',
+    'pic': 'West.png'
+  },
 ];
 
 class StateList extends StatefulWidget {
@@ -86,7 +187,10 @@ class _StateListState extends State<StateList> {
                         ? TextButton.icon(
                             onPressed: () {
                               Navigator.pushNamed(context, '/details',
-                                  arguments: {'index': states[idx]['index']});
+                                  arguments: {
+                                    'index': states[idx]['index'],
+                                    'state': states[idx]['state']
+                                  });
                             },
                             icon: Icon(Icons.adjust_outlined),
                             label: Text(">"))
